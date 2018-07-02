a2a3a2186c52ea06f8c9bf5d30e68031f4417951
rahulanand4110 <rahulanand4110@gmail.com>
2018-07-02 17:10:07 +0530

    Added Cricket Option

--- newsweb/app/news_api.py
+++ newsweb/app/news_api.py
@@ -1,20 +1,25 @@
 from newsapi import NewsApiClient
+from datetime import date, timedelta
 
-
+today_date = date.today().strftime("%Y-%m-%d")
+yesterday_date = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
+newsapi = NewsApiClient(api_key='API_KEY')
 class NewsApi(object):
-	newsapi = NewsApiClient(api_key='API_KEY')
-	top_headlines = newsapi.get_top_headlines(country='in')
+	
+	top_headlines = newsapi.get_top_headlines(country='in'or'us',page=1)
 	
 	description = []
 	url = []
 	image_link = []
 	title = []
+	sources = []
 	
 	for article in top_headlines['articles']:
 		description.append(article['description'])
 		url.append(article['url'])
 		image_link.append(article['urlToImage'])
 		title.append(article['title'])
+		sources.append(article['source']['name'])
 	
 	def get_discription(self):
 		return self.description
@@ -27,6 +32,28 @@ class NewsApi(object):
 
 	def get_title(self):
 		return self.title
+
+	def get_source(self):
+		return self.sources
+
+
+class CricketNews(NewsApi):
+	get_every = newsapi.get_everything(q="Cricket",
+											from_parameter=yesterday_date,	
+											to=today_date,
+											language='en',
+											sort_by='relevancy',
+											page=2)
+	description = []
+	url = []
+	image_link = []
+	title = []
+	
+	for article in get_every['articles']:
+		description.append(article['description'])
+		url.append(article['url'])
+		image_link.append(article['urlToImage'])
+		title.append(article['title'])
 
 
 
--- newsweb/app/routes.py
+++ newsweb/app/routes.py
@@ -10,6 +10,19 @@ def index():
 	urls = news.get_url()
 	images = news.get_image_link()
 	title = news.get_title()
+	sources = news.get_source()
 
 	n = len(descriptions)
-	return render_template('index.html',title='News Web', descriptions=descriptions, urls=urls, images=images,length=n, get_title=title)
+	return render_template('index.html',title='News Web', descriptions=descriptions, urls=urls, images=images,length=n, get_title=title,sources=sources)
+
+@app.route('/cricket')
+def get_cricket_news():
+	news = news_api.CricketNews()
+	descriptions = news.get_discription()
+	urls = news.get_url()
+	images = news.get_image_link()
+	title = news.get_title()
+	sources = news.get_source()
+
+	n = len(descriptions)
+	return render_template('index.html',title='News Web', descriptions=descriptions, urls=urls, images=images,length=n, get_title=title,sources=sources)
